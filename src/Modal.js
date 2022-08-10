c87fc597a6cdf2542a33752cd8ebfde000d6abaa
Kunhee Kim <kunheekim@korea.ac.kr>
2022-08-11 04:29:59 +0900

    Feat: FireStore for saving users

--- src/Modal.js
+++ src/Modal.js
@@ -1,4 +1,43 @@
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "./lib/firebase_config";
+
 const Modal = ({ closeModal }) => {
+  const [number, setNumber] = useState("");
+  const [agree, setAgree] = useState(false);
+  const [beta, setBeta] = useState(false);
+
+  const handleNumber = (e) => {
+    const regex = /^[0-9\b -]{0,15}$/;
+    if (regex.test(e.target.value)) {
+      setNumber(e.target.value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!agree) {
+      alert("전화번호 수집에 동의해주세요.");
+      return;
+    }
+    if (number == "") {
+      alert("전화번호를 입력해 주세요.");
+      return;
+    }
+
+    const moivesCollRef = collection(db, "users");
+    addDoc(moivesCollRef, { number, beta })
+      .then((response) => {
+        console.log(response);
+        alert("사전예약이 완료되었습니다! 곧 만나요!");
+        closeModal();
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("오류가 발생했습니다. 다시 시도해주세요.");
+      });
+  };
+
   return (
     <>
       <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
@@ -22,13 +61,21 @@ const Modal = ({ closeModal }) => {
                   <input
                     class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
                     type="text"
+                    value={number}
+                    onChange={handleNumber}
+                    required
                   />
                 </div>
               </div>
               <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/4"></div>
                 <label class="md:w-1/2 block text-gray-500 font-bold">
-                  <input class="mr-2 leading-tight" type="checkbox" />
+                  <input
+                    class="mr-2 leading-tight"
+                    type="checkbox"
+                    value={agree}
+                    onChange={() => setAgree(!agree)}
+                  />
                   <span class="text-sm">
                     (필수) 정식 출시 알림을 위한 전화번호 수집에 동의합니다.
                   </span>
@@ -37,7 +84,12 @@ const Modal = ({ closeModal }) => {
               <div class="md:flex md:items-center mb-6">
                 <div class="md:w-1/4"></div>
                 <label class="md:w-1/2 block text-gray-500 font-bold">
-                  <input class="mr-2 leading-tight" type="checkbox" />
+                  <input
+                    class="mr-2 leading-tight"
+                    type="checkbox"
+                    value={beta}
+                    onChange={() => setBeta(!beta)}
+                  />
                   <span class="text-sm">
                     (선택) 베타 테스터 선정에 응모합니다.
                   </span>
@@ -55,7 +107,7 @@ const Modal = ({ closeModal }) => {
               <button
                 className="bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                 type="button"
-                onClick={closeModal}
+                onClick={handleSubmit}
               >
                 제출하기
               </button>
